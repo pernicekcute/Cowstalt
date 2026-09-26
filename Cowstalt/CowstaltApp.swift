080ff1d9ddac9818e6ca32f8577f23856d65a972
Ondra <pernicekcute@gmail.com>
2026-09-26 08:11:22 +0200

    Update CowstaltApp.swift

--- Cowstalt/CowstaltApp.swift
+++ Cowstalt/CowstaltApp.swift
@@ -2,6 +2,11 @@ import SwiftUI
 
 @main
 struct CowstaltApp: App {
+init() {
+        // Set the delegate when the app launches
+        UNUserNotificationCenter.current().delegate = NotificationDelegate.shared
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
